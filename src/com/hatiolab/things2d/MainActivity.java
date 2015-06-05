d78668ed7648246fbeefa0603e6cfc547b8fd2c0
heartyoh <heartyoh@gmail.com>
2015-06-05 20:19:57 +0900

    import ..

--- src/com/hatiolab/things2d/MainActivity.java
+++ src/com/hatiolab/things2d/MainActivity.java
@@ -13,7 +13,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
 import com.hatiolab.things2d.dxhost.ThingsHost;
